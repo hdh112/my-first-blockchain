d6c05abf7bd4cb981d1e3915f2afbff7b3e8a1eb
hdh112 <avohdh112@gmail.com>
2020-04-20 17:42:56 +0900

    Resolved sync issue on new apps.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
 
@@ -7,6 +8,10 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+// Test broadcast function
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
@@ -26,7 +31,18 @@ app.post('/api/mine', (req, res) => {
     res.redirect('/api/blocks');
 });
 
-const DEFAULT_PORT = 3000;
+// Sync among multiple blockchains
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+        if (!error && response.statusCode===200) {
+            const rootChain = JSON.parse(body);
+
+            console.log(`Syncing with root blockchain:`, rootChain);
+            blockchain.replaceChain(rootChain);
+        };
+    });
+};
+
 let PEER_PORT;
 
 // cf) window.crypto.getRandomValues() for better safety
@@ -36,4 +52,7 @@ if (process.env.GENERATE_PEER_PORT === 'true') {
 
 // Set up server (localhost)
 const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`listening at localhost:${PORT}`);
+    syncChains();
+});
